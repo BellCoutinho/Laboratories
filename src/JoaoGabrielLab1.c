96dcef0d840696a213d547239d1fa1563392951d
Bell Coutinho <bell.sccoutinho@gmail.com>
2019-09-11 15:51:50 -0300

    Adicionado as opções 1 e 5 do menu

---
+++ src/JoaoGabrielLab1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main (int argc, char** argv)
+{
+    int opcao_de_menu;
+    int opcao;
+    bool sair = false;
+
+    do 
+    {
+        puts("====================================");
+        printf("1. Sequência de fibonacci\n");
+        printf("2. Sequência de termos\n");
+        printf("3. Operações aritméticas\n");
+        printf("4. Dobras\n");
+        printf("5. Sair do programa\n");
+        puts("====================================");
+
+        printf("Digite sua escolha: ");
+        scanf("%d", &opcao_de_menu);
+
+        switch (opcao_de_menu)
+        {
+            case 1:
+                printf("Digite um número positivo: ");
+                scanf("%d", &opcao);
+
+                if (opcao < 0)
+                    puts("0");
+                else if (opcao == 0)
+                    puts("0 1");
+                else 
+                {
+                    int tmp1 = 0;
+                    int tmp2 = 1;
+                    int soma = 0;
+
+                    printf("0 1 ");
+                    while (soma <= opcao)
+                    {
+                        soma = tmp1 + tmp2;
+                        tmp1 = tmp2;
+                        tmp2 = soma;
+                        printf("%d ", soma);
+                    }
+                    puts("");
+                }
+                break;
+            case 5:
+                sair = true;
+                puts("Saindo do programa...");
+                break;
+                
+        }
+
+    } while (!sair);
+
+    return EXIT_SUCCESS;
+}
